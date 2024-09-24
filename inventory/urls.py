5905d500a721f4c55e94f109f9a0961d94f8d701
asu2sh <asu2shy@gmail.com>
2024-09-25 02:55:05 +0530

    added url names for item endpoints

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('items/', ItemView.as_view()),
-    path('items/<int:item_id>', ItemView.as_view()),
+    path('items/', ItemView.as_view(), name='item_list'),
+    path('items/<int:item_id>', ItemView.as_view(), name='item_detail'),
 
     path('auth/register/', RegisterView.as_view(), name='register_user'),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
