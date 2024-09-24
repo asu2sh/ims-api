2baaa3e56782cb359c53f665b5b820bf0e6ce045
asu2sh <asu2shy@gmail.com>
2024-09-25 02:05:49 +0530

    env config for secret key

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hip%h@v+s*f0bhy+mgypxi^!9k^u%g+gu0^ji%p5=^)mjrues%'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-hip%h@v+s*f0bhy+mgypxi^!9k^u%g+gu0^ji%p5=^)mjrues%')
 
 # Deubg Mode
 DEBUG = config('DEBUG', default=False, cast=bool)
