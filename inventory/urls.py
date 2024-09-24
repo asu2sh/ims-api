74d714139ee55ac363b401e008831482770aa453
asu2sh <asu2shy@gmail.com>
2024-09-25 01:13:38 +0530

    added JWT token generation/auth endpoints

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ItemView
+from .views import ItemView, RegisterView
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('items/', ItemView.as_view()),
     path('items/<int:item_id>', ItemView.as_view()),
+
+    path('auth/register/', RegisterView.as_view(), name='register_user'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
