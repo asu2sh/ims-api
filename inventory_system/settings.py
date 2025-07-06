033fe361929c9940da08442b4a69dd95cfbb8c30
Ashutosh Kumar <asu2shy@gmail.com>
2025-07-06 20:28:30 +0530

    Update settings.py

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'inventory_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'inventory_system', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
